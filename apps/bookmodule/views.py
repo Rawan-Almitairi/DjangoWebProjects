9eefe4451740c87f8ddc69af7bff59ffbb47b7eb
Pixel <rawan@email.com>
2026-02-15 22:01:44 +0300

    Lab3

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    # return HttpResponse("Hello, world!")
+    name = request.GET.get("name") or "world!"
+    # return HttpResponse("Hello, "+name)
+    return render(request, "bookmodule/index.html", {"name": name})
+def index2(request, val1 = 0):
+    return HttpResponse("value1 = "+str(val1))
+def viewbook(request, bookId):
+    book1 = {
+        "id": 123,
+        "title": "continuous delivery",
+        "author": "J. Humble and D. Farley",
+    }
+    book2 = {
+        "id": 456,
+        "title": "Secret of Reverse Engineering",
+        "author": "E. Eilam",
+    }
+    targetBook = None
+    if book1["id"] == bookId:
+        targetBook = book1
+    if book2["id"] == bookId:
+        targetBook = book2
+    context = {"book": targetBook}
+    return render(request, "bookmodule/show.html", context)
+    
